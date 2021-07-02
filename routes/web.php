d6e6a5894c9914adf03dac9c03c354cfc3d22875
psa <psaproxy@gmail.com>
2021-07-02 15:58:59 +0300

    Удаление отладки

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    app()->make(\Core\Counter\Actions\IncValueOffset::class)->execute('banner');
     return view('welcome');
 });
 
